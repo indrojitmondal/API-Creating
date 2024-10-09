94dbc06b1a41db5e8fa0235db140f0493d8c6e26
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-09 17:40:19 +0600

    added new data

---
+++ js/api.js
@@ -0,0 +1,7 @@
+console.log('Connected API');
+const apiTesting = async()=>{
+    const res = await fetch('https://indrojitmondal.github.io/API-Creating/');
+    const data = await res.json();
+    console.log(data)
+}
+apiTesting();
