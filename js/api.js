4918a09476df8044bc1e3c98bea3e63456c7601e
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-09 18:10:48 +0600

    succesfully API created

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
-console.log('Connected API');
-const apiTesting = async()=>{
-    const res = await fetch('https://indrojitmondal.github.io/API-Creating/');
+console.log('Connected my API');
+const apITesting = async ()=>{
+    const res= await fetch('https://indrojitmondal.github.io/API-Creating/');
     const data = await res.json();
     console.log(data);
 }
-apiTesting();
+apITesting();
